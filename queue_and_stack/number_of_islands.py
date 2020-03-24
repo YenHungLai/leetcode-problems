c5e6ce36297ef0e9c78abc2313edc03eabe13d63
YenHungLai <jacoblai1029@gmail.com>
2020-03-24 02:31:23 -0700

    More

--- queue_and_stack/number_of_islands.py
+++ queue_and_stack/number_of_islands.py
@@ -20,3 +20,41 @@ class Solution:
         self.dfs(row, col - 1, grid)
         self.dfs(row + 1, col, grid)
         self.dfs(row - 1, col, grid)
+
+
+input = [["1", "1", "1", "1", "0"],
+         ["1", "1", "0", "1", "0"],
+         ["1", "1", "0", "0", "0"],
+         ["0", "0", "0", "0", "0"]]
+
+
+def numIslands(grid) -> int:
+    """BFS"""
+    count, queue = 0, []
+
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
+            if grid[row][col] == '1':
+                count += 1
+                grid[row][col] == '0'
+                queue.append((row, col))
+                while queue:
+                    r, c = queue.pop(0)
+                    # Push all neighbors.
+                    if r + 1 <= len(grid) - 1 and grid[r+1][c] == '1':
+                        queue.append((r + 1, c))
+                        grid[r+1][c] = '0'
+                    if r - 1 >= 0 and grid[r-1][c] == '1':
+                        queue.append((r - 1, c))
+                        grid[r-1][c] = '0'
+                    if c + 1 <= len(grid[0]) - 1 and grid[r][c+1] == '1':
+                        queue.append((r, c + 1))
+                        grid[r][c+1] = '0'
+                    if c - 1 >= 0 and grid[r][c-1] == '1':
+                        queue.append((r, c - 1))
+                        grid[r][c-1] = '0'
+
+    return count
+
+
+print(numIslands(input))
