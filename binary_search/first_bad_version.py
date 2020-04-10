62a7a304a217cd258887a0b7eccfc68a92334216
YenHungLai <jacoblai1029@gmail.com>
2020-04-10 10:48:08 -0700

    More

---
+++ binary_search/first_bad_version.py
@@ -0,0 +1,11 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+
+def firstBadVersion(n):
+    left, right = 0, len(n)
+
+
+print(firstBadVersion(5))
